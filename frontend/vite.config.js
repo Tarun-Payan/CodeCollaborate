9881607ec92716fd55c41db8ff2e7a57a2bb5888
Tarun-Payan <tarunpayan8@gmail.com>
2025-03-30 16:14:09 +0530

    allow vite app to access from other devices

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    host: '0.0.0.0', // Allow access from the network
+    port: 5173, // Change if needed
+  }
 })
